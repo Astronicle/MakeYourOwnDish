327a95293220c72510f135e5436b61dcb8d26354
Abdul Rehman <astronicle78@gmail.com>
2025-02-04 12:44:49 +0530

    Added conditional rendering for getting recipe

--- src/components/IngredientsAdd.jsx
+++ src/components/IngredientsAdd.jsx
@@ -1,22 +1,40 @@
+import { use } from 'react'
 import './components.css'
 import {useState} from "react"
 
 function IngredientsAdd() {
 
     const [ingredientList, setIngredientList] = useState([])
+    const [recipeShown, setRecipeShown] = useState(false)
 
     function addIngredient(formData) {
         const ingredientInput = formData.get("ingredient")
-        setIngredientList(prevIngredientList => [...prevIngredientList, ingredientInput])
+        if (!(ingredientInput == ""))
+            setIngredientList(prevIngredientList => [...prevIngredientList, ingredientInput])
     }
 
     const ingredientListJsx = ingredientList.map((x, _) => <li key={_}>{x}</li>)
 
     function checkAvailability() {
         if (ingredientList.length!=0)
-            return <div className='ingredientHeader'>Ingredients added-</div>
+            return <div className='ingredientHeader1'>Ingredients added-</div>
             else 
-            return <div className='ingredientHeader'>No ingredients to display</div>
+            return <div className='ingredientHeader2'>Add ingredients to start generating recipes!</div>
+    }
+
+    function checkRecipeCount() {
+        if (ingredientList.length>2)
+            return <div className='recipeContainer1'>
+                        <div className='getRecipeDescriptionContainer'>
+                            <h3>Ready for a recipe?</h3>
+                            <p>Generate a recipe from the entered list of ingredients.</p>
+                        </div>
+                        <button>Get Recipe</button>
+                    </div>
+        else if(ingredientList.length>0)
+            return <div className='recipeContainer2'>
+                        <p>3 items required to generate, add them in!</p>
+                   </div>
     }
 
     return(
@@ -31,9 +49,12 @@ function IngredientsAdd() {
                 </form>
             </div>
             {checkAvailability()}
-            <ul className='listContainer'>
-                {ingredientListJsx}
-            </ul>
+            <div className='contentContainer'>
+                <ul className='listContainer'>
+                    {ingredientListJsx}
+                </ul>
+                {checkRecipeCount()}
+            </div>
         </>
     )
 }
